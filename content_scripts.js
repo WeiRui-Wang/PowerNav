259503708b9fc5cd2431f8cfaeff33660f086509
WeiRui Wang <wnwang@ncsu.edu>
2023-03-05 16:58:34 -0500

    Updated content_scripts query selector logic to be more inclusive closed #4

--- content_scripts.js
+++ content_scripts.js
@@ -12,13 +12,26 @@ function keydownEvent(event) {
     highlight(keyword);
 }
 
+function getGradientColor(steps, step) {
+    const yellow = [255, 255, 0];
+    const orange = [255, 165, 0];
+
+    const ratio = step % steps / (steps - 1);
+
+    const red = Math.round(yellow[0] + (orange[0] - yellow[0]) * ratio);
+    const green = Math.round(yellow[1] + (orange[1] - yellow[1]) * ratio);
+    const blue = Math.round(yellow[2] + (orange[2] - yellow[2]) * ratio);
+
+    return `rgb(${red},${green},${blue})`;
+}
+
 function highlight(keyword) {
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
         const elementInnerText = element.innerText.toLowerCase();
 
         if (elementInnerText.startsWith(keyword.toLowerCase())) {
-            element.style.backgroundColor = 'orange';
+            element.style.backgroundColor = getGradientColor(elements.length, i);
         }
         else {
             element.style.backgroundColor = '';
