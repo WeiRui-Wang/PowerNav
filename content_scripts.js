4d1f72e44508d78e08049393de2111255c737feb
WeiRui Wang <wnwang@ncsu.edu>
2023-03-06 00:50:34 -0500

    Implemented dynamic color gradient with keyboard input search, close #3

--- content_scripts.js
+++ content_scripts.js
@@ -6,17 +6,23 @@ if (document.readyState !== 'loading') {
     // }
 }
 
+var keyword = '';
+var total = 0;
+
 document.addEventListener('keydown', keydownEvent);
 function keydownEvent(event) {
-    const keyword = event.key;
-    highlight(keyword);
+    keyword += event.key;
+    let highlightRet = highlight(keyword);
+    if (keyword.length == 1) {
+        total = highlightRet;
+    }
 }
 
-function getGradientColor(steps, step) {
+function getGradientColor(current, total) {
     const yellow = [255, 255, 0];
     const orange = [255, 165, 0];
 
-    const ratio = step % steps / (steps - 1);
+    const ratio = 1 - current / total;
 
     const red = Math.round(yellow[0] + (orange[0] - yellow[0]) * ratio);
     const green = Math.round(yellow[1] + (orange[1] - yellow[1]) * ratio);
@@ -26,14 +32,27 @@ function getGradientColor(steps, step) {
 }
 
 function highlight(keyword) {
+    var current = 0;
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
         const elementInnerText = element.innerText.toLowerCase();
         if (elementInnerText.startsWith(keyword.toLowerCase())) {
-            element.style.backgroundColor = getGradientColor(elements.length, i);
+            current++;
+        }
+    }
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const elementInnerText = element.innerText.toLowerCase();
+        if (elementInnerText.startsWith(keyword.toLowerCase())) {
+            if (total == 0) {
+                total = current;
+            }
+            element.style.backgroundColor = getGradientColor(current, total);
+            console.log(`element.style.backgroundColor = getGradientColor($current, $total);` + current + "TOTAL" + total);
         }
         else {
             element.style.backgroundColor = '';
         }
     }
+    return current;
 }
