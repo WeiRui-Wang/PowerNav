bfdc6763d3eacd0bfd1a215ede2c1f195429bcd1
WeiRui Wang <wnwang@ncsu.edu>
2023-03-06 15:37:54 -0500

    Updated README.md, content_script.js focus() action

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # PowerNav
 
 PowerNav is a Chrome extension that facilitates navigation across compatible websites for accessibility and power user.
-
-Idea was publicly proposed during brainstorming of [DiamondHacks '22](https://github.com/mshirsekar1/DiamondHacks22/blob/master/README.md).
+With current design, exntension will auto focus on the first matched element within all detected elements within the page.
--- content_scripts.js
+++ content_scripts.js
@@ -1,28 +1,43 @@
 var elements;
 if (document.readyState !== 'loading') {
-    elements = document.querySelectorAll('a[href][class*="nav"], section[class*="bar"] a[href], div[class*="nav"] a[href], li a[href], ul[id*="nav"] li');
+    elements = document.querySelectorAll('a[href][class*="nav"], section[class*="bar"] a[href], div[class*="nav"] a[href], li a[href], ul[id*="nav"] li a[href]');
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor = 'yellow';
     }
 }
 
 var keyword = '';
+var matchedElement;
 
 document.addEventListener('keydown', keydownEvent);
 function keydownEvent(event) {
     keyword += event.key;
     highlight(keyword);
+    if (event.key === 'Enter' && matchedElement) {
+        keyword = '';
+        matchedElement.click();
+        console.log('clicked fired' + matchedElement);
+    }
 }
 
 function highlight(keyword) {
+    var firstElement;
     for (let i = 0; i < elements.length; i++) {
         const element = elements[i];
         const elementInnerText = element.innerText.toLowerCase();
         if (elementInnerText.startsWith(keyword.toLowerCase())) {
             element.style.backgroundColor = 'orange';
+            if (!firstElement) {
+                firstElement = element;
+            }
         }
         else {
             element.style.backgroundColor = 'yellow';
         }
+    }
+    if (firstElement) {
+        console.log(firstElement);
+        firstElement.focus();
+        matchedElement = firstElement;
     }
 }
