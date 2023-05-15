74220e661f3b86b8750550ae79ff00ade225c182
wnwang <wnwang@ncsu.edu>
2023-05-15 17:14:20 -0400

    Updated content_scripts and permissions requirements in manifest.json.

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 PowerNav allows users to quickly navigate to and interact with navigation menu elements on most website, enhancing the productivity for accessibility and power users.
 
+PowerNav is a Chrome extension designed to facilitate efficient and accessible web navigation using keyboard inputs. By searching for, highlighting, and navigating to page elements based on user-entered keywords, PowerNav enhances the browsing experience for power users and individuals who rely on keyboard navigation due to accessibility requirements.
+
+Usage
+
+Once installed, PowerNav automatically begins functioning on all web pages, targeting elements such as links within sections, divs, lists, or any other element that has a "nav" in its class or ID. When a user starts typing, the tool looks for elements that start with the entered keyword.
+
+The following keyboard events are of importance:
+
+1. Typing Characters (A-Z, Space): As you type, PowerNav starts searching for matching elements on the page based on the keyword entered. The first element that matches the keyword will be focused and stored as the "matched" element.
+
+2. Enter Key: Pressing the "Enter" key when there's a matched element will navigate to (or 'click') the matched element.
+
+Please note that PowerNav ignores keystrokes when focus is on text input fields or text areas to prevent unintended navigation. Also, the tool ignores modifier keys (Alt, Ctrl, Meta, Shift).
+
+Timeouts
+
+PowerNav uses timeouts to reset the keyword and matched element. If a key isn't pressed within 456 milliseconds, the keyword is reset. Similarly, if no matching element is found within 789 milliseconds, the matched element state is reset.
+
+Visual Feedback
+
+In the current version of the script, visual feedback (changing the background color of the matched elements) is commented out. However, if uncommented, it will highlight all potential navigation targets in yellow and the currently matched element in orange. This can provide an easier visual reference to users.
+
+Note: This tool is designed for accessibility and power users and may require a basic understanding of keyboard navigation principles to be used effectively. Always make sure to double-check the target of your navigation before hitting the "Enter" key.
+
 ![Marquee promo tile](https://user-images.githubusercontent.com/58345470/224576466-5a902f1c-8abf-413b-bd56-6b7c62a23878.png)
 
 ![PowerNav](https://user-images.githubusercontent.com/58345470/224559818-0e99decc-59e0-4c9d-a240-b80bd0ed9c25.gif)
--- content_scripts.js
+++ content_scripts.js
@@ -9,96 +9,100 @@ var targetElements;
 
 // When the window is loaded, highlight all the target elements
 window.addEventListener("load", (event) => {
-    targetElements = document.querySelectorAll('a[href][class*="nav"], section[class*="bar"] a[href], div[class*="nav"] a[href], li a[href], ul[id*="nav"] li a[href]');
-    for (var i = 0; i < targetElements.length; i++) {
-        // targetElements[i].style.backgroundColor = 'yellow';
-    };
+  targetElements = document.querySelectorAll(
+    'a[href][class*="nav"], section[class*="bar"] a[href], div[class*="nav"] a[href], li a[href], ul[id*="nav"] li a[href]'
+  );
+  for (var i = 0; i < targetElements.length; i++) {
+    // targetElements[i].style.backgroundColor = 'yellow';
+  }
 });
 
 // Initialize keyword and matchedElement
-var keyword = '';
+var keyword = "";
 var matchedElement;
 
 // matched flag
 var matched = false;
 
 // Handle keydown events
-document.addEventListener('keydown', keydownEvent);
+document.addEventListener("keydown", keydownEvent);
 function keydownEvent(event) {
-    // Check if current focus is on a text input field or text area to prevent unintended navigation
-    if (event.target.tagName.toLowerCase() === 'input' || event.target.tagName.toLowerCase() === 'textarea') {
-        return;
+  // Check if current focus is on a text input field or text area to prevent unintended navigation
+  if (
+    event.target.tagName.toLowerCase() === "input" ||
+    event.target.tagName.toLowerCase() === "textarea"
+  ) {
+    return;
+  }
+  // Check if the key is a modifier key and ignore input
+  if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
+    return;
+  }
+  // Only take character keys
+  if (event.keyCode == 32 || (event.keyCode >= 65 && event.keyCode <= 90)) {
+    // Append the key to the keyword
+    keyword += event.key;
+    if (matched) {
+      // Highlight the matched element if there's a match
+      highlight(keyword, matchedElement);
+    } else {
+      // Highlight all the target elements if there's no match yet
+      highlight(keyword, targetElements);
     }
-    // Check if the key is a modifier key and ignore input
-    if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
-        return;
-    }
-    // Only take character keys
-    if (event.keyCode == 32 || (event.keyCode >= 65 && event.keyCode <= 90)) {
-        // Append the key to the keyword
-        keyword += event.key;
-        if (matched) {
-            // Highlight the matched element if there's a match
-            highlight(keyword, matchedElement);
-        } else {
-            // Highlight all the target elements if there's no match yet
-            highlight(keyword, targetElements);
-        }
-    }
-    // Handle enter key press
-    if (event.key === 'Enter' && matchedElement) {
-        // Reset the keyword and click the matched element
-        keyword = '';
-        matchedElement.click();
-    }
+  }
+  // Handle enter key press
+  if (event.key === "Enter" && matchedElement) {
+    // Reset the keyword and click the matched element
+    keyword = "";
+    matchedElement.click();
+  }
 }
 
 // Set up timeouts for keyup events and target element events
 let timeout;
 let targetTimeout;
-document.addEventListener('keyup', keyupEvent);
+document.addEventListener("keyup", keyupEvent);
 function keyupEvent(event) {
-    // Clear the timeout for keyup event
-    clearTimeout(timeout);
-    // Set a new timeout to reset the keyword and matched flag
-    timeout = setTimeout(() => {
-        keyword = '';
-        matched = true;
-    }, KEYUP_TIMEOUT_LENGTH);
-    // Clear the timeout for target matched state
-    clearTimeout(targetTimeout);
-    // Set a new timeout to reset the matched element and keyword
-    targetTimeout = setTimeout(() => {
-        matchedElement = null;
-        matched = false;
-        keyword = '';
-    }, TARGET_TIMEOUT_LENGTH);
+  // Clear the timeout for keyup event
+  clearTimeout(timeout);
+  // Set a new timeout to reset the keyword and matched flag
+  timeout = setTimeout(() => {
+    keyword = "";
+    matched = true;
+  }, KEYUP_TIMEOUT_LENGTH);
+  // Clear the timeout for target matched state
+  clearTimeout(targetTimeout);
+  // Set a new timeout to reset the matched element and keyword
+  targetTimeout = setTimeout(() => {
+    matchedElement = null;
+    matched = false;
+    keyword = "";
+  }, TARGET_TIMEOUT_LENGTH);
 }
 
 // Highlight the matching elements
 function highlight(keyword, elements) {
-    var firstElement;
-    if (!elements) {
-        return;
+  var firstElement;
+  if (!elements) {
+    return;
+  }
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const elementInnerText = element.innerText.toLowerCase();
+    if (elementInnerText.startsWith(keyword.toLowerCase())) {
+      // Change the background color to orange if there's a match
+      // element.style.backgroundColor = 'orange';
+      if (!firstElement && element.checkVisibility()) {
+        firstElement = element;
+      }
+    } else {
+      // Change the background color to yellow if there's no match
+      // element.style.backgroundColor = 'yellow';
     }
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        const elementInnerText = element.innerText.toLowerCase();
-        if (elementInnerText.startsWith(keyword.toLowerCase())) {
-            // Change the background color to orange if there's a match
-            // element.style.backgroundColor = 'orange';
-            if (!firstElement && element.checkVisibility()) {
-                firstElement = element;
-            }
-        }
-        else {
-            // Change the background color to yellow if there's no match
-            // element.style.backgroundColor = 'yellow';
-        }
-    }
-    // Focus on the first matching element and store it as matchedElement
-    if (firstElement) {
-        firstElement.focus();
-        matchedElement = firstElement;
-    }
+  }
+  // Focus on the first matching element and store it as matchedElement
+  if (firstElement) {
+    firstElement.focus();
+    matchedElement = firstElement;
+  }
 }
